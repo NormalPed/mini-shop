43cbf578a82d5c9cdb53d60b1e66e588ccf4e80c
aboba <maksshos05@gmail.com>
2023-11-24 18:04:44 +0200

    items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      items:[
+        {
+          id: 1,
+          title: 'Grey Chair',
+          img: 'chair-grey.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          category: 'chairs',
+          price: '49.99'
+        },
+        {
+          id: 2,
+          title: 'Table',
+          img: 'table.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          category: 'tables',
+          price: '149.00'
+        },
+        {
+          id: 3,
+          title: 'Sofa',
+          img: 'sofa.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          category: 'sofa',
+          price: '549.99'
+        },
+        {
+          id: 4,
+          title: 'Lamp',
+          img: 'wall-light.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          category: 'light',
+          price: '25.00'
+        },
+        {
+          id: 5,
+          title: 'White Chair',
+          img: 'chair-white.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          category: 'chairs',
+          price: '49.99'
+        },
+      ]
+    }
+  }
+  render(){
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items}/>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
